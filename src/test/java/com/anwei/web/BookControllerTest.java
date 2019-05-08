ab5338062706a370000d9d9db34efbbc1ed39b8b
anweicloud <d7210c@163.com>
2019-05-08 18:02:57 +0800

    @Rollback

--- src/test/java/com/anwei/web/BookControllerTest.java
+++ src/test/java/com/anwei/web/BookControllerTest.java
@@ -14,18 +14,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * 
- * @author Kemin
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 // 配置事务的回滚,对数据库的增删改都会回滚,便于测试用例的循环利用
-@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Rollback
 @Transactional
 public class BookControllerTest extends AbstractContextControllerTests {
 
